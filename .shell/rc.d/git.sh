6fa7524a6ac278f39ede19ffd93a1529479c6551
DancingQuanta <andytheseeker@gmail.com>
2016-09-27 11:32:27 +0100

    Added bfg cleaner to git

--- .shell/rc.d/git.sh
+++ .shell/rc.d/git.sh
@@ -7,3 +7,21 @@ else
 	echo "Either .git not found or the git repo is not locked"
 fi
 }
+
+# Github hub
+alias git=hub
+
+# BFG repo-cleaner
+BFG=$HOME/bin/bfg.jar
+bfg() {
+  echo $BFG
+  if [ -f $BFG ]; then
+    if [[ $OSTYPE == "linux" ]]; then
+      java -jar $BFG $@
+    elif [[ $OSTYPE == "cygwin" ]]; then
+      java -jar $(cygpath -w -a $BFG) $@
+    fi
+  else
+    echo "Ensure bfg.jar is in ~/bin/"
+  fi
+}
