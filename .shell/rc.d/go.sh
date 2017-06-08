a24e5b718abcd71ddbdf03979eb905738565aade
DancingQuanta <andytheseeker@gmail.com>
2017-06-08 18:58:12 +0100

    Make go environment for cygwin

--- .shell/rc.d/go.sh
+++ .shell/rc.d/go.sh
@@ -2,7 +2,11 @@
 
 # Go environment
 
-export GOPATH=$HOME/go
+#export GOPATH=$HOME/go
+if [[ $OSTYPE == "cygwin" ]]; then
+  export GOPATH=$(cygpath -aw $HOME/go)
+fi
+
 prepend_path $GOROOT/bin
 prepend_path $GOPATH/bin
 
