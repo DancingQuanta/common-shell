c37eeba60f1a26ae8bd277e0bae659d296ccb299
DancingQuanta <andytheseeker@gmail.com>
2017-07-17 15:15:01 +0100

    make sure OGPATH is exported in non-cygwin distro

--- .shell/rc.d/go.sh
+++ .shell/rc.d/go.sh
@@ -2,9 +2,10 @@
 
 # Go environment
 
-#export GOPATH=$HOME/go
 if [[ $OSTYPE == "cygwin" ]]; then
   export GOPATH=$(cygpath -aw $HOME/go)
+else
+  export GOPATH=$HOME/go
 fi
 
 prepend_path $GOROOT/bin
