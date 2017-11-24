b588e3c037cbeda672f96ba46d53490cac9520ed
DancingQuanta <andytheseeker@gmail.com>
2017-11-24 09:20:31 +0000

    More refined control of agents

--- .config/shell/profile.d/50-agents.sh
+++ .config/shell/profile.d/50-agents.sh
@@ -1,10 +1,14 @@
 ## DancingQuanta/shell-config - https://github.com/DancingQuanta/shell-config
 ## 50-agents.sh
-## This file sets up SSH and GPG agentsfor the shell
+## This file sets up SSH and GPG agents for the shell
 
-if which keychain &> /dev/null; then
-  eval $(keychain --eval --agents gpg,ssh ~/.ssh/master/*id_rsa 1E8030A9)
-elif which ssh-agent &> /dev/null; then
+if [[ -x $(command -v keychain) ]]; then
+  if [[ -x $(command -v gpg2) ]]; then
+    eval $(keychain --eval --agents gpg,ssh --gpg2 ~/.ssh/master/*id_rsa 1E8030A9)
+  else
+    eval $(keychain --eval --agents gpg,ssh ~/.ssh/master/*id_rsa 1E8030A9)
+  fi
+elif [[ -x $(command -v ssh-agent) ]]; then
   SSHAGENT=$(which ssh-agent)
   SSHAGENTARGS="-s"
   if [ -z "$SSH_AUTH_SOCK" -a -x "$SSHAGENT" ]; then
@@ -12,4 +16,3 @@ elif which ssh-agent &> /dev/null; then
     trap "kill $SSH_AGENT_PID" 0
   fi
 fi
-
